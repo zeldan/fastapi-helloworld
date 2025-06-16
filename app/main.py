cd80524976f98d6256e75ee9ae471973de7c8616
Daniel Zelei <zelei.dan@gmail.com>
2025-06-16 23:38:29 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def read_root():
     if "HELLOWORLD_ENV" in environ:
         txt = environ.get('HELLOWORLD_ENV')
     else:
-        txt = "HELLOWORLD_ENV not found!"
+        txt = "HELLOWORLD_ENV not found"
     return {"HELLOWORLD_ENV: {}".format(txt): "from /one/hello"}
 
 
